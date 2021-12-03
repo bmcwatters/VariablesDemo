3a6f6c4af929b329bde5588fcd4ee8193f3a248e
bryan mcwatters <bmcwatters@comcast.net>
2021-12-03 11:28:59 -0500

    VarInitCommit

---
+++ VariablesDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Sample Variables
+
+string firstName = "BillyBob";
+int age = 32;
+//bool isMale = true;
+char midInit = 'E';
+double distance = 200.34;
+decimal myDec = 15.22235M;
+
+Console.WriteLine($"Hi, my name is {firstName}. I'm {age} years old. My middle initial is {midInit}.");
+Console.WriteLine($"The distance to my house is {distance} miles and my favorite decimal is {myDec}.");
+
+
